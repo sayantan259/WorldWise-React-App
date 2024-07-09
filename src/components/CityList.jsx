d73e414da7dba86dbf72b46eff9034aee72bffa9
Sayantan Bairagi <sayantan259@gmail.com>
2024-07-09 10:22:23 +0530

    Some improvements done

---
+++ src/components/CityList.jsx
@@ -0,0 +1,21 @@
+import Cityitem from "./Cityitem";
+import styles from "./CityList.module.css";
+import Spinner from "./Spinner";
+import Message from "./Message";
+
+function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return <Message message="Add your first city from the map" />;
+
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <Cityitem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
+}
+
+export default CityList;
